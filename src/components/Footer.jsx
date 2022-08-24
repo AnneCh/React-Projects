f5b0212651ec969f918acff91057427de58fe78c
Anne Chretien <chretien.an@gmail.com>
2022-08-24 17:58:52 +0200

    finished styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import '../styles.css';
+import git from "./github.png"
 
 //insert social icons, twitter, facebook,github
 
 export default function Footer(){
     return(
+        <>
+        <p className="divider">_______________________________</p>
         <div className="footer">
-            <p className="footer-text">I'm your footer.</p>
+            <p className="footer-text">See my work : </p>
+            <btn><img className="git" src={git}></img></btn>
         </div>
+        </>
     )
 }
