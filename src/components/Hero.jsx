c5bbcce6953547548b3dada073a5eea23211f4fd
Anne Chretien <chretien.an@gmail.com>
2022-08-30 13:39:39 +0200

    hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles.css';
+import grid from "./photoss.png";
+
+export default function Hero(){
+    return(
+        <div className="hero">
+            <img className="hero-pics" src={grid} alt="experiences-grid"></img>
+            <h1 className="hero-h1">Online Experiences</h1>
+            <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts - all without leaving home.</p>
+        </div>
+    )
+}
