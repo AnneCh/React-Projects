8332b9e2272625f1315eb4da86867a0dddbd9467
Anne Chretien <chretien.an@gmail.com>
2022-09-08 09:11:52 +0200

    flex display

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import grid from "./photoss.png";
 
 export default function Hero(){
     return(
-        <section>
+        <section className="hero">
             <img className="hero-pics" src={grid} alt="experiences-grid"></img>
             <h1 className="hero-h1">Online Experiences</h1>
             <p className="hero-text">Join unique interactive activities led by one-of-a-kind hosts - all without leaving home.</p>
