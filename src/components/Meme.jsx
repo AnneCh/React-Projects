2334cac724df2fb69c80dbf03efc5112d4b7151e
Anne Chretien <chretien.an@gmail.com>
2022-09-30 17:36:34 +0200

    image display

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import '../styles.css';
-import MemeResult from "./MemeResult"
-import data from "../memesData.js"
 import memesData from "../memesData";
-
 
 export default function Meme(){
 
     const [memeImage, setMemeImage] = React.useState("")
+
     function handleClick(){  
         const memesArray = memesData.data.memes 
         const randomNumber = Math.floor(Math.random() * memesArray.length)
@@ -16,12 +14,14 @@ export default function Meme(){
 
     return(
         <main>
-            <form>
+            <div className="form">
                 <input className="form-input" type="text" placeholder="Say the words..."></input>
                 <input className="form-input" type="text" placeholder="...and I'll print them"></input>
                 <button onClick={handleClick} className="form-button">Generate New Meme!!</button>
-                <div><img src={memeImage}/></div>
-            </form>
+            </div>
+            <div  className="meme-image">
+                <img src={memeImage}/>
+            </div>
         </main>
     )
 }
--- src/components/MemeResult.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import '../styles.css'
-
-export default function MemeResult(props){
-    <div className="meme-image">
-        <image src={props.data.memes.url}/>
-    </div>
-}
